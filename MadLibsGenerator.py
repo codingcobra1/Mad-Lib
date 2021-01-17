086960e5dc1bb3bc3757c623e9577e0e01d8d5fc
CodingCobra1 <codingcobra1@gmail.com>
2021-01-17 06:39:29 +0530

    First commit

---
+++ MadLibsGenerator.py
@@ -0,0 +1,9 @@
+place_1 = input("Please enter a place: ")
+noun_1 = input("Please enter a noun: ")
+noun_2 = input("Please enter another noun: ")
+noun_3 = input("Please enter a different noun from the previous ones: ")
+
+print("I had to go to " + place_1 + ".")
+print("When we got there, my kind counselor shouted loudly, 'Everybody get off " + noun_1 + ".'")
+print("I saw a(n) " + noun_2 + " jumping up and down the tree.")
+print("I got some peanuts and passed  them through the cage to a gigantic gray " + noun_3 + " towering above my head.")
